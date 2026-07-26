7dc23459d8ad519ecf53f6055672b4b5b8b59748
Yuvraj Gupta <your-email@example.com>
2026-07-27 02:19:59 +0530

    add Bootstrap navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,14 @@
+
+
 function Header() {
     return (
-        <header>
-            <h1>Student Management System</h1>
-        </header>
+        <nav className="navbar navbar-dark bg-primary">
+            <div className="container">
+        <span className="navbar-brand mb-0 h1">
+          Student Management System
+        </span>
+            </div>
+        </nav>
     );
 }
 
